230999dcff27003d3fb4a857262f43d0065e1bb7
Cory <cscharf91@gmail.com>
2025-06-09 21:17:45 -0400

    feat: add bounce shroom and respawn checkpoint nodes, update tile map data, and refine bounce behavior

--- Scripts/Components/bounce.gd
+++ Scripts/Components/bounce.gd
@@ -14,8 +14,4 @@ func _on_body_entered(body: Node) -> void:
         
         var current_bounce_velocity = bounce_velocity if bounce_velocity > 0 else PlayerConfig.bounce_velocity
         body.bounce(current_bounce_velocity)
-        
-        PlayerConfig.current_air_dashes = PlayerConfig.max_air_dashes
-        PlayerConfig.current_jumps = PlayerConfig.max_jumps - 1
-        PlayerConfig.current_frisbee_throws = PlayerConfig.max_frisbee_throws
-        # TODO - handle releasing jump key slow-mo
+        body.handle_landing()
